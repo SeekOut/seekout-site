d9e0a83f8218981eed2480543c131c0f74869fff
Taylor Ho <taylorkmho@gmail.com>
2016-06-20 10:55:14 -0700

    Init commit

---
+++ src/assets/js/app.js
@@ -0,0 +1,8 @@
+// ------------------------------------------
+
+// External Libraries
+
+// ------------------------------------------
+
+// Get rid of no-js
+document.querySelector('html').classList.remove('no-js');
