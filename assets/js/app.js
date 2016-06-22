7157fcee5aad2aa078bf501568c1c165613ff240
Taylor Ho <taylorkmho@gmail.com>
2016-06-21 19:30:23 -0700

    Update 2016-06-22T02:30:21.191Z

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,8 +5,36 @@
 
 // ------------------------------------------
 
-// Get rid of no-js
-document.querySelector('html').classList.remove('no-js');
+jQuery(function ($) {
+  $('.slider--testimonials').slick({
+    'fade': true,
+    'zIndex': 200,
+    'prevArrow': '<img src="assets/images/svg/ui-nav-pag.svg"/>',
+    'nextArrow': '<img src="assets/images/svg/ui-nav-pag.svg"/>',
+    'arrows': true,
+    'dots': false,
+    'responsive': [{
+      'breakpoint': 767,
+      'settings': {
+        'arrows': false,
+        'dots': true
+      }
+    }]
+  });
+
+  /*
+    GLOBAL smooth scroll
+  */
+
+  $('[href="#anchor--newsletter"]').click(function () {
+    $('html, body').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1000, 'swing', function () {
+      $('.mailform__input--text').focus();
+    });
+    return false;
+  });
+});
 
 },{}]},{},[1])
 
