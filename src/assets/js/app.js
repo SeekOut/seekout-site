bc319c153d4c93d77e331c43910dd2dd52a8d758
Taylor Ho <taylorkmho@gmail.com>
2016-06-21 18:50:19 -0700

    Responsive testimonial

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -9,6 +9,17 @@ jQuery(function($){
     'fade': true,
     'zIndex': 200,
     'prevArrow': '<img src="assets/images/svg/ui-nav-pag.svg"/>',
-    'nextArrow': '<img src="assets/images/svg/ui-nav-pag.svg"/>'
+    'nextArrow': '<img src="assets/images/svg/ui-nav-pag.svg"/>',
+    'arrows': true,
+    'dots': false,
+    'responsive': [
+      {
+        'breakpoint': 767,
+        'settings': {
+          'arrows': false,
+          'dots': true
+        }
+      }
+    ]
   });
 });
