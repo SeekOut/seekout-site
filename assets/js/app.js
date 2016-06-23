264783180445f253ae11b6ba88b4ba7f4be87346
Taylor Ho <taylorkmho@gmail.com>
2016-06-23 10:48:39 -0700

    Update 2016-06-23T17:48:37.243Z

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,7 +12,7 @@ jQuery(function ($) {
     'prevArrow': '<img src="assets/images/svg/ui-nav-pag.svg"/>',
     'nextArrow': '<img src="assets/images/svg/ui-nav-pag.svg"/>',
     'arrows': true,
-    'dots': false,
+    'dots': true,
     'responsive': [{
       'breakpoint': 767,
       'settings': {
@@ -21,6 +21,53 @@ jQuery(function ($) {
       }
     }]
   });
+
+  if (matchMedia) {
+    var tsMediaQuery = window.matchMedia("(max-width: 767px)");
+    tsMediaQuery.addListener(tsCallback);
+    tsCallback(tsMediaQuery);
+  }
+  function tsCallback(tsMediaQuery) {
+    if ($('.slider--testimonials').hasClass('slick-initialized')) $('.slider--testimonials').slick('unslick');
+    var slickOptions;
+    if (tsMediaQuery.matches) {
+      slickOptions = {
+        'fade': true,
+        'zIndex': 200,
+        'arrows': false,
+        'dots': true
+      };
+    } else {
+      slickOptions = {
+        'fade': true,
+        'zIndex': 200,
+        'prevArrow': '<img src="assets/images/svg/ui-nav-pag.svg"/>',
+        'nextArrow': '<img src="assets/images/svg/ui-nav-pag.svg"/>',
+        'arrows': true,
+        'dots': true
+      };
+    }
+    $('.slider--testimonials').slick(slickOptions);
+  }
+
+  if (matchMedia) {
+    var mqPostEventSlider = window.matchMedia("(max-width: 419px)");
+    mqPostEventSlider.addListener(postEventSliderCallback);
+    postEventSliderCallback(mqPostEventSlider);
+  }
+  function postEventSliderCallback(mqPostEventSlider) {
+    if ($('.slider--post-your-event').hasClass('slick-initialized')) $('.slider--post-your-event').slick('unslick');
+    if (mqPostEventSlider.matches) {
+      $('.slider--post-your-event').slick({
+        'fade': true,
+        'zIndex': 200,
+        'prevArrow': '<img src="assets/images/svg/ui-nav-pag.svg"/>',
+        'nextArrow': '<img src="assets/images/svg/ui-nav-pag--white.svg"/>',
+        'arrows': true,
+        'dots': false
+      });
+    }
+  }
 
   /*
     GLOBAL smooth scroll
@@ -32,6 +79,12 @@ jQuery(function ($) {
     }, 1000, 'swing', function () {
       $('.mailform__input--text').focus();
     });
+    return false;
+  });
+
+  $(".popup-link").click(function () {
+    var window_size = this.getAttribute('data-popup-size');
+    window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,' + window_size);
     return false;
   });
 });
